419b9588650a0d33b7926e7d0329672e3d665769
umairansari <umairansari.work@gmail.com>
2024-11-28 04:22:59 +0500

    feat: add user signin method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,12 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 import { SignupDto } from './dto/signup.dto';
+import { SigninDto } from './dto/signin.dto';
 
 @Injectable()
 export class AuthService {
@@ -20,6 +26,31 @@ export class AuthService {
       statusCode: 201,
       message: 'User signed up successfully',
       user: newUser,
+    };
+  }
+
+  async signin(signinDto: SigninDto) {
+    const foundUser = await this.userService.findByEmail(signinDto.email);
+
+    if (!foundUser) {
+      throw new UnauthorizedException('User with this email does not exist');
+    }
+
+    const isPasswordMatch = await bcrypt.compare(
+      signinDto.password,
+      foundUser.password,
+    );
+
+    if (!isPasswordMatch) {
+      throw new UnauthorizedException('Incorrect password');
+    }
+
+    delete foundUser.password;
+
+    return {
+      statusCode: 200,
+      message: 'User signed in successfully',
+      user: foundUser,
     };
   }
 }
