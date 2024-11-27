323c0fa614ce5d70792dcd8cc2508d4d5b6ab687
umairansari <umairansari.work@gmail.com>
2024-11-27 10:59:07 +0500

    feat: add email conflict check during user signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { SignupDto } from './dto/signup.dto';
 
@@ -7,10 +7,18 @@ export class AuthService {
   constructor(private readonly userService: UserService) {}
 
   async signup(signupDto: SignupDto) {
+    const existingUser = await this.userService.findByEmail(signupDto.email);
+
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists');
+    }
+
     const newUser = await this.userService.create(signupDto);
+    delete newUser.password;
 
     return {
-      message: 'Success',
+      statusCode: 201,
+      message: 'User signed up successfully',
       user: newUser,
     };
   }
