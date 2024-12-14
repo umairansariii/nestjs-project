db043bae19bb67bbd8b1e6518035888d5f818107
umairansari <umairansari.work@gmail.com>
2024-12-14 06:06:45 +0500

    feat: add required and type exception

--- src/auth/dto/change-password.dto.ts
+++ src/auth/dto/change-password.dto.ts
@@ -1,14 +1,26 @@
-import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 
 export class ChangePasswordDto {
   @IsNotEmpty({ message: 'Password should not empty' })
   @IsString({ message: 'Password must be a string' })
+  @IsDefined({ message: 'Password is required' })
   oldPassword: string;
 
   @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W).+$/, {
     message: 'Password is not strong enough',
+    context: {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+    },
   })
   @MinLength(8, { message: 'Password must be at least 8 characters' })
   @IsString({ message: 'Password must be a string' })
+  @IsDefined({ message: 'Password is required' })
   newPassword: string;
 }
--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,11 +1,13 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsDefined, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SigninDto {
   @IsEmail(undefined, { message: 'Email is not valid address' })
   @IsNotEmpty({ message: 'Email should not empty' })
+  @IsDefined({ message: 'Email is required' })
   email: string;
 
   @IsNotEmpty({ message: 'Password should not empty' })
   @IsString({ message: 'Password must be a string' })
+  @IsDefined({ message: 'Password is required' })
   password: string;
 }
